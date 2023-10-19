66db63122414b43c6646c328df23c0e1e179dc6c
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-19 15:32:01 +0200

    Add project files.

---
+++ MAUIVERTER/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIVERTER
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
